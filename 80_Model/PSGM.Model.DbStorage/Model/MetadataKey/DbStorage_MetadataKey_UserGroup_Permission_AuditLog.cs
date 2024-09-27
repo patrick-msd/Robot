8262dde9eb719557be44e324af3653de01b66d64
Patrick Schoenegger <p.schoenegger@readcoop.eu>
2024-09-27 15:16:02 +0200

    Update

--- 80_Model/PSGM.Model.DbStorage/Model/MetadataKey/DbStorage_MetadataKey_UserGroup_Permission_AuditLog.cs
+++ 80_Model/PSGM.Model.DbStorage/Model/MetadataKey/DbStorage_MetadataKey_UserGroup_Permission_AuditLog.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PSGM.Model.DbStorage
 {
-    [Table("file_UserGroup_Permission_AuditLog")]
+    [Table("MetadataKey_UserGroup_Permission_AuditLog")]
     public class DbStorage_MetadataKey_UserGroup_Permission_AuditLog
     {
         #region Entities
